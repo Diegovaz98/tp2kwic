8fc17e4542e9a9f6954a5b045b1248a0826b22fc
diegovaz98 <diego.vazfernandes@hotmail.com>
2019-09-30 16:18:03 -0300

    Primeiro commit

---
+++ kiwic-TP2-2019/main.rb
@@ -0,0 +1,42 @@
+
+module Sorting
+
+    def sort_Alphabet
+        
+    end
+
+end    
+
+module Search
+
+    def searching
+
+    end
+end
+
+class DblpAPI
+    
+        include GetInput
+        
+        def acessar
+        end
+    end
+    
+    class Arquivo
+        def acessar
+        end
+    end
+    
+    
+    puts "Escolha entre\n [1] Arquivo txt\n [2]API(por favor não)"
+    escolha = gets.chomp.to_i      		
+
+    
+    case escolha
+        
+    when 1
+        # Tratar arquivo
+    when 2
+        # tratar API
+    end
+    
---
+++ kiwic-TP2-2019/test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative './main'
+
+class SortingTest < Minitest::Test
+
+    def test_is_alphabetical?
+        
+        #testar se sai ordem alfabetica
+    end 
+end
